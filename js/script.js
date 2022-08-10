f37dcde2d15421fbd42da43782ec8ba839ab839d
rachidbend <rachidbendaghor@gmail.com>
2022-08-10 17:09:38 +0100

    adjusted gallery, testimonials, and hero header

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@
 
 const btnNavEl = document.querySelector(".btn--nav");
 const bodyEl = document.querySelector("body");
-console.log(btnNavEl);
-console.log(bodyEl);
+const mainNavEl = document.querySelector(".nav--main");
+
+const outsidClick = function (e) {
+  if (!e.target.closest(".nav--main") && !e.target.closest(".btn--nav")) {
+    bodyEl.classList.remove("nav-open");
+  }
+};
 
 btnNavEl.addEventListener("click", (e) => {
+  // Preventing default behavior
   e.preventDefault();
+  // toggling class on body to open or close nav
   bodyEl.classList.toggle("nav-open");
-  console.log("btn clicked");
+
+  // closing nav if body is clicked, excluding the nav itself and nav button
+  bodyEl.addEventListener("click", outsidClick);
+  // bodyEl.removeEventListener("click", outsidClick);
 });
